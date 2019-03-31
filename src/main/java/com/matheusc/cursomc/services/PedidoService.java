606d8b324808c4ce9c0ce9b1e44fa6fdebff5692
Matheus Castro <matheuscastro@gmail.com>
2019-03-31 14:36:20 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/matheusc/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.matheusc.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.matheusc.cursomc.domain.Pedido;
+import com.matheusc.cursomc.repositories.PedidoRepository;
+import com.matheusc.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
