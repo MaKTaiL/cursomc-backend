65763ef8fe1c0d8f425094ec4547be1d0c4ef421
Matheus Castro <matheuscastro@gmail.com>
2019-04-04 10:44:55 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/com/matheusc/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/matheusc/cursomc/resources/CategoriaResource.java
@@ -39,7 +39,7 @@ public class CategoriaResource {
 		Categoria obj = service.fromDTO(objDto);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-				.path("/{id}").buildAndExpand(objDto.getId()).toUri();
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
 	
--- src/main/java/com/matheusc/cursomc/services/ClienteService.java
+++ src/main/java/com/matheusc/cursomc/services/ClienteService.java
@@ -10,9 +10,15 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.matheusc.cursomc.domain.Cidade;
 import com.matheusc.cursomc.domain.Cliente;
+import com.matheusc.cursomc.domain.Endereco;
+import com.matheusc.cursomc.domain.enums.TipoCliente;
 import com.matheusc.cursomc.dto.ClienteDTO;
+import com.matheusc.cursomc.dto.ClienteNewDTO;
+import com.matheusc.cursomc.repositories.CidadeRepository;
 import com.matheusc.cursomc.repositories.ClienteRepository;
+import com.matheusc.cursomc.repositories.EnderecoRepository;
 import com.matheusc.cursomc.services.exceptions.DataIntegrityException;
 import com.matheusc.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -20,24 +26,35 @@ import com.matheusc.cursomc.services.exceptions.ObjectNotFoundException;
 public class ClienteService {
 	
 	@Autowired
-	private ClienteRepository repo;
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public Cliente find(Integer id) {
-		Optional<Cliente> obj = repo.findById(id);
+		Optional<Cliente> obj = clienteRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
 	
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = clienteRepository.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
+	}
+	
 	public Cliente update(Cliente obj) {
 		find(obj.getId());
-		return repo.save(obj);
+		return clienteRepository.save(obj);
 	}
 	
 	public void delete(Integer id) {
 		find(id);
 		
 		try {
-			repo.deleteById(id);
+			clienteRepository.deleteById(id);
 		}
 		catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possivel excluir porque há entidades relacionadas");
@@ -45,12 +62,12 @@ public class ClienteService {
 	}
 	
 	public List<Cliente> findAll() {
-		return repo.findAll();
+		return clienteRepository.findAll();
 	}
 	
 	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-		return repo.findAll(pageRequest);
+		return clienteRepository.findAll(pageRequest);
 	}
 	
 	public Cliente fromDTO(ClienteDTO objDto) {
@@ -60,5 +77,35 @@ public class ClienteService {
 				objDto.getEmail(), 
 				find(objDto.getId()).getCpfOuCnpj(), 
 				find(objDto.getId()).getTipo());
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(
+				null, 
+				objDto.getNome(), 
+				objDto.getEmail(), 
+				objDto.getCpfOuCnpj(), 
+				TipoCliente.toEnum(objDto.getTipo()));
+		
+		Cidade cid = cidadeRepository.getOne(objDto.getCidadeId());
+		
+		Endereco end = new Endereco(
+				null, 
+				objDto.getLogradouro(), 
+				objDto.getNumero(), 
+				objDto.getComplemento(), 
+				objDto.getBairro(), 
+				objDto.getCep(), cli, cid);
+		
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if(objDto.getTelefone2()!=null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if(objDto.getTelefone3()!=null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		
+		return cli;
 	}
 }
