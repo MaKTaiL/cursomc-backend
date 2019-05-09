5c1f83ae2758a19cf8d4b54cd1e44061918bc3ac
Matheus Castro <matheuscastro@gmail.com>
2019-05-09 11:11:11 -0300

    Enviando imagem via endpoint

--- src/main/java/com/matheusc/cursomc/services/ClienteService.java
+++ src/main/java/com/matheusc/cursomc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.matheusc.cursomc.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.matheusc.cursomc.domain.Cidade;
 import com.matheusc.cursomc.domain.Cliente;
@@ -39,6 +41,8 @@ public class ClienteService {
 	private CidadeRepository cidadeRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private S3Service s3Service;
 	
 	public Cliente find(Integer id) {
 		UserSS user = UserService.authenticated();
@@ -120,5 +124,9 @@ public class ClienteService {
 		}
 		
 		return cli;
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 }
